939aabc1324eebaede312297ff6a98e88e52342b
hee <monghee0727@gmail.com>
2022-02-21 16:09:06 +0900

    css update
    image size complete

--- js/background.js
+++ js/background.js
@@ -1,13 +1,11 @@
 const images = [
   "jaeA.jpg",
   "jaeB.jpg",
-  "jaeC.jpg",
-  "jaeD.png",
+  "jaeC.png",
+  "jaeD.jpg",
   "jaeE.jpg",
   "jaeF.jpg",
   "jaeG.jpg",
-  "jaeH.jpg",
-  "jaeI.jpg",
 ];
 
 // 랜덤으로 이미지 가져오기
